2edfc399c326cb1e5a24f95a07d10eb1b16ebbaf
DarrenCT <99516347+DarrenCT@users.noreply.github.com>
2025-08-28 08:48:04 +0800

    Implement removeFromArray function and enable tests
    Added the removeFromArray function to remove specified elements from an array. Updated the test suite by enabling all relevant test cases to verify correct behavior for multiple scenarios.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+function removeFromArray(inputArr, ...toBeRemoved) {
+    let output = [];
+    let match = false;
+    for(let i = 0; i < inputArr.length; i++){
+        for(let j = 0; j < toBeRemoved.length; j++){
+            if(inputArr[i] === toBeRemoved[j]){
+                match = true;
+                break;
+            }
+        }
+        if(!match){
+            output.push(inputArr[i]);
+        }
+        match = false;
+    }
+    return output;
 };
 
 // Do not edit below this line
